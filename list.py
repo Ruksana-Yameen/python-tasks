815c4161383c16906438b4d26b1188a0f3d00d30
Ruksana-Yameen <108459066+Ruksana-Yameen@users.noreply.github.com>
2023-05-15 10:55:18 +0300

    Add files via upload

---
+++ list.py
@@ -0,0 +1,15 @@
+# Create a list of names
+names = ["Alice", "Bob", "Charlie", "David"]
+
+# Append a new name to the list
+names.append("Eve")
+
+# Remove a name from the list
+names.remove("Charlie")
+
+# Sort the list in alphabetical order
+names.sort()
+
+# Iterate over the list and print each name
+for name in names:
+    print(name)
