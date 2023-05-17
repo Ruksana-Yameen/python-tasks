be63ea616c210bd3087166d4a8aa895f5612fc9f
Ruksana-Yameen <108459066+Ruksana-Yameen@users.noreply.github.com>
2023-05-17 09:15:14 +0300

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,15 @@
+# Create a dictionary of ages
+ages = {"Alice": 25, "Bob": 30, "Charlie": 35, "David": 40}
+
+# Accessing keys
+print("Keys:", ages.keys())
+
+# Accessing values
+print("Values:", ages.values())
+
+# Accessing key-value pairs
+print("Key-Value Pairs:", ages.items())
+
+# Iterate over the dictionary and print each key-value pair
+for key, value in ages.items():
+    print(key, ":", value)
